b36fb21adcb12cf9b3d854447f4ced1e3e019bac
Ys199610 <1605066010@qq.com>
2019-09-07 15:33:26 +0800

    third comment 增加了产品子页面，修改了子页面上方导航功能

--- ej/js/index.js
+++ ej/js/index.js
@@ -4,6 +4,47 @@ $(function(){
 				var url = $(this).attr("url");
 				$(this).addClass("current");
 				$(this).siblings().removeClass("current");
+				// if(url ===  "./pages/customer.html" ){
+				// 	$(".mb_nav").className = "customer";
+				// 	$(".customer").css("display","block");
+				// 	$(".customer").siblings().css("display","none");
+				// }else if(url === "./pages/waiter.html"){
+				// 	$(".mb_nav").className = "waiter";
+				// 	$(".waiter").css("display","block");
+				// 	$(".waiter").siblings().css("display","none");
+				// };
+				switch(url){
+					case "./pages/customer.html":
+						$(".mb_nav").className = "customer";
+						$(".customer").css("display","block");
+						$(".customer").siblings().css("display","none");
+						break;
+					case "./pages/waiter.html":
+						$(".mb_nav").className = "waiter";
+						$(".waiter").css("display","block");
+						$(".waiter").siblings().css("display","none");
+						break;
+					case "./pages/category.html":
+						$(".mb_nav").className = "category";
+						$(".category").css("display","block");
+						$(".category").siblings().css("display","none");
+						break;
+					case "./pages/product.html":
+						$(".mb_nav").className = "product";
+						$(".product").css("display","block");
+						$(".product").siblings().css("display","none");
+						break
+					case "./pages/order.html":
+						$(".mb_nav").className = "order";
+						$(".order").css("display","block");
+						$(".order").siblings().css("display","none");
+						break;
+					case "./pages/comment.html":
+						$(".mb_nav").className = "comment";
+						$(".comment").css("display","block");
+						$(".comment").siblings().css("display","none");
+						break;
+				}
 				$("#wrapper").load(url);
 
 			}
